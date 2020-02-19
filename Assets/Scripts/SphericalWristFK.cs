01b25451179e4632b137339fe3af1aa739db90e1
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-02-19 19:28:50 +0200

    added ui fk

--- Assets/Scripts/SphericalWristFK.cs
+++ Assets/Scripts/SphericalWristFK.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class SphericalWristFK : MonoBehaviour
 {
+    [Header("Game Objects")]
     [SerializeField] Transform manBase = null;
     [SerializeField] Transform joint0 = null;
     [SerializeField] Transform joint1 = null;
@@ -12,12 +13,18 @@ public class SphericalWristFK : MonoBehaviour
     [SerializeField] Transform endEffector = null;
     [SerializeField] Transform prismaticJoint = null;
 
+    [Header("Texts")]
     [SerializeField] Text a1Text = null;
     [SerializeField] Text a2Text = null;
     [SerializeField] Text a3Text = null;
     [SerializeField] Text d3Text = null;
     [SerializeField] Text theta1Text = null;
     [SerializeField] Text theta2Text = null;
+
+    [Header("End Effector")]
+    [SerializeField] Text endEffectorXText = null;
+    [SerializeField] Text endEffectorYText = null;
+    [SerializeField] Text endEffectorZText = null;
 
     private float a1, a2, a3;
     private float theta1, theta2;
@@ -29,35 +36,31 @@ public class SphericalWristFK : MonoBehaviour
 
     void Start()
     {
-        Init();
+        InitParams();
+        InitPositions();
         SetTable();
         SetTexts();
-
-        H0_1 = GetMatrix(theta1, 90.0f, 0.0f, a1);
-        H1_2 = GetMatrix(theta2 + 90.0f, 90.0f, 0.0f, 0.0f);
-        H2_3 = GetMatrix(0.0f, 0.0f, 0.0f, a2 + a3 + d3);
-
-        H0_3 = H0_1 * H1_2 * H2_3;
-
-        Debug.Log("Col 0 " + H0_3.GetColumn(0));
-        Debug.Log("Col 1 " + H0_3.GetColumn(1));
-        Debug.Log("Col 2 " + H0_3.GetColumn(2));
-        Debug.Log("Col 3 " + H0_3.GetColumn(3));
+        SetDH();
+        SetEndEffectorText();
     }
 
-    private void Init()
+    private void InitParams()
     {
+        table = new float[3][];
+        table[0] = new float[4];
+        table[1] = new float[4];
+        table[2] = new float[4];
+
         a1 = joint1.position.y - manBase.position.y;
         a2 = joint2.position.x - joint1.position.x;
         a3 = endEffector.position.x - joint2.position.x;
-        theta1 = 0.0f;
-        theta2 = 70.0f;
+        theta1 = 25.0f;
+        theta2 = 56.0f;
         d3 = 2.0f;
+    }
 
-        Debug.Log(a1);
-        Debug.Log(a2);
-        Debug.Log(a3);
-
+    private void InitPositions()
+    {
         prismaticJoint.position = new Vector3(prismaticJoint.position.x + d3, prismaticJoint.position.y, prismaticJoint.position.z);
         joint1.rotation = Quaternion.AngleAxis(90.0f - theta2, -Vector3.forward);
         joint0.rotation = Quaternion.AngleAxis(theta1, -Vector3.up);
@@ -65,13 +68,8 @@ public class SphericalWristFK : MonoBehaviour
 
     private void SetTable()
     {
-        table = new float[3][];
-        table[0] = new float[4];
-        table[1] = new float[4];
-        table[2] = new float[4];
-
         table[0][0] = theta1;
-        table[1][0] = theta2;
+        table[1][0] = theta2 + 90.0f;
         table[2][0] = 0.0f;
 
         table[0][1] = 90.0f;
@@ -85,6 +83,32 @@ public class SphericalWristFK : MonoBehaviour
         table[0][3] = a1;
         table[1][3] = 0.0f;
         table[2][3] = a2 + a3 + d3;
+    }
+
+    private void SetTexts()
+    {
+        a1Text.text = "A1: " + a1.ToString();
+        a2Text.text = "A2: " + a2.ToString();
+        a3Text.text = "A3: " + a3.ToString();
+        d3Text.text = "D3: " + d3.ToString();
+        theta1Text.text = "Theta1: " + ((int)(theta1)).ToString();
+        theta2Text.text = "Theta2: " + ((int)(theta2)).ToString();
+    }
+
+    private void SetDH()
+    {
+        H0_1 = GetMatrix(table[0][0], table[0][1], table[0][2], table[0][3]);
+        H1_2 = GetMatrix(table[1][0], table[1][1], table[1][2], table[1][3]);
+        H2_3 = GetMatrix(table[2][0], table[2][1], table[2][2], table[2][3]);
+
+        H0_3 = H0_1 * H1_2 * H2_3;
+    }
+
+    private void SetEndEffectorText()
+    {
+        endEffectorXText.text = H0_3.GetColumn(3).x > 0.0001 ? "X " + H0_3.GetColumn(3).x.ToString() : "X " + 0;
+        endEffectorYText.text = H0_3.GetColumn(3).y > 0.0001 ? "Y " + H0_3.GetColumn(3).y.ToString() : "Y " + 0;
+        endEffectorZText.text = H0_3.GetColumn(3).z > 0.0001 ? "Z " + H0_3.GetColumn(3).z.ToString() : "Z " + 0;
     }
 
     private Matrix4x4 GetMatrix(float theta, float alpha, float r, float d)
@@ -106,13 +130,5 @@ public class SphericalWristFK : MonoBehaviour
         return angle * Mathf.PI / 180.0f;
     }
 
-    private void SetTexts()
-    {
-        a1Text.text = "A1: " + a1.ToString();
-        a2Text.text = "A2: " + a2.ToString();
-        a3Text.text = "A3: " + a3.ToString();
-        d3Text.text = "D3: " + d3.ToString();
-        theta1Text.text = "Theta1: " + theta1.ToString();
-        theta2Text.text = "Theta2: " + theta2.ToString();
-    }
+    
 }

195185192c170f00ec1eb2451160fba5e20f0b57
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-03-17 17:09:37 +0200

    added fk logic

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    void Start()
+    {
+        FindBaseValues(1.15f, -2.22f, 90.0f);
+    }
+
+    private void FindBaseValues(float x, float y, float angle)
+    {
+        float angleRadians = FromDegreesToRadians(angle);
+
+        Vector2 vec1 = new Vector2(x, y);
+        Vector2 vec2 = new Vector2(Mathf.Cos(angleRadians) * vec1.x - Mathf.Sin(angleRadians) * vec1.y,
+            Mathf.Cos(angleRadians) * vec1.y + Mathf.Sin(angleRadians) * vec1.x);
+        Vector2 vec3 = new Vector2(Mathf.Cos(angleRadians) * vec2.x - Mathf.Sin(angleRadians) * vec2.y,
+            Mathf.Cos(angleRadians) * vec2.y + Mathf.Sin(angleRadians) * vec2.x);
+        Debug.Log("Vec1: " + vec1.ToString("F2"));
+        Debug.Log("Vec2: " + vec2.ToString("F2"));
+        Debug.Log("Vec3: " + vec3.ToString("F2"));
+    }
+
+    private float FromDegreesToRadians(float theta)
+    {
+        return theta * Mathf.PI / 180.0f;
+    }
+}
---
+++ Assets/Scripts/SP - FK/Socket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class Socket : MonoBehaviour
+{
+    // Use this for initialization
+    internal Boolean socketReady = false;
+    TcpClient mySocket;
+    NetworkStream theStream;
+    StreamWriter theWriter;
+    StreamReader theReader;
+    String Host = "localhost";
+    Int32 Port = 5500;
+
+    void Start()
+    {
+        setupSocket();
+        Debug.Log("socket is set up");
+    }
+
+    public void setupSocket()
+    {
+        try
+        {
+            mySocket = new TcpClient(Host, Port);
+            theStream = mySocket.GetStream();
+            theWriter = new StreamWriter(theStream);
+            socketReady = true;
+            //writeSocket("yah!! it works");
+
+            //Byte[] sendBytes = Encoding.UTF8.GetBytes("yah!! it works");
+            //mySocket.GetStream().Write(sendBytes, 0, sendBytes.Length);
+
+            String message = "cee";
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            theStream.Write(data, 0, data.Length);
+
+            Debug.Log("socket is sent");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e);
+        }
+    }
+}

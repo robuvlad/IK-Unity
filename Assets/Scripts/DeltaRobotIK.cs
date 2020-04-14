71fd54af8ad73fab15fa22792ac9c512e14b3cc9
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-04-14 19:43:53 +0300

    added delta robot first setup

---
+++ Assets/Scripts/DeltaRobotIK.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeltaRobotIK : MonoBehaviour
+{ 
+    [SerializeField] Transform[] topVerticesPoints = null;
+    [SerializeField] Transform[] BPoints = null;
+    [SerializeField] Transform[] PPoints = null;
+
+    void Start()
+    {
+        Debug.Log(" " + (BPoints[1].position - BPoints[2].position).magnitude);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        DrawLine(BPoints[0].position, BPoints[1].position, BPoints[2].position);
+        Gizmos.color = Color.green;
+        DrawLine(topVerticesPoints[0].position, topVerticesPoints[1].position, topVerticesPoints[2].position);
+        Gizmos.color = Color.red;
+        DrawLine(PPoints[0].position, PPoints[1].position, PPoints[2].position);
+    }
+
+    private void DrawLine(Vector3 vec1, Vector3 vec2, Vector3 vec3)
+    {
+        Gizmos.DrawLine(vec1, vec2);
+        Gizmos.DrawLine(vec2, vec3);
+        Gizmos.DrawLine(vec3, vec1);
+    }
+}
